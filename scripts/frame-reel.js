3c81572495f13507e69e01e485b249117721bae6
Veniamin Lebedev <145424000+venyalebedev@users.noreply.github.com>
2025-07-17 17:59:54 +0400

    Add files via upload

---
+++ scripts/frame-reel.js
@@ -0,0 +1,25 @@
+let frameWrapper = document.querySelector('#frame-wrapper')
+let frameReel = document.querySelector('#frame-reel')
+
+// Создать переменную с именем проекта
+// Добавить цикл для назначения условий: чтобы скрипт применялся только для
+
+let frameCount = 14
+
+for (let i = 1; i <= frameCount; i = i + 1) {
+  let image = document.createElement('img')
+  image.src = `images/pravdinsk-identity/cover-sequence/pravdinsk-cover-${i}.jpg`
+
+  frameReel.append(image)
+}
+
+frameWrapper.addEventListener('mousemove', function(event) {
+  let rect = frameWrapper.getBoundingClientRect()
+
+  let progress = (event.clientX - rect.left) / (rect.width)
+  let frameNumber = (Math.floor(progress * frameCount))
+
+  frameNumber = Math.max(frameNumber, 0)
+
+  frameReel.style.marginLeft = -100 * frameNumber + '%'
+})
